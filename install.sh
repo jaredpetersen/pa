63be5272cb6453105c6a4b8cea0207df8c6acfcc
Jared Petersen <jpetersen11@wou.edu>
2015-10-02 11:38:04 -0700

    Install script now fits within width reqs

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 
 echo "Installing Personal Assistant..."
 sudo cp ./pa/pa /usr/local/bin && \
-( sudo mkdir /var/pa ; sudo cp ./pa/commands ./LICENSE ./README.md /var/pa ) && \
+(sudo mkdir /var/pa ; sudo cp ./pa/commands ./LICENSE ./README.md /var/pa) && \
 export PATH=$PATH:/usr/local/bin && \
 echo "Installation Complete"
